0b0a263da52df28d50df343071e0acf65653a265
SYADRINA-XI-TJKT-3 <sarden082@gmail.com>
2025-08-04 15:11:53 +0700

    update fitur register

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { RegisterPage } from "../api/apiUrls.js";
+// import { RegisterPage } from "../api/apiUrls";
 
 class auth {
   async RegisterPage(dataRegister) {
