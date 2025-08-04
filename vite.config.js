d8cc60fa065ecda62fdbde7b112c3321ec737bb1
SYADRINA-XI-TJKT-3 <sarden082@gmail.com>
2025-08-04 11:45:57 +0700

    Fix: bersihin konflik di vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,8 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-  alias: {
-  }
-  }
-});
-
-=======
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()]
 })
->>>>>>> fitur/login
