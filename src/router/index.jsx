65ba14a1b988b7da4fb175d82e1b59fc4e00a3cd
SYADRINA-XI-TJKT-3 <sarden082@gmail.com>
2025-08-04 11:43:21 +0700

    Merge forgotpassword feature

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPages from "../Pages/LoginPages";
 import HomePages from "../Pages/HomePages";
+import RegisterPage from "../pages/RegisterPages";
+import { ForgotPassword } from "../api/apiUrls";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPages />} />
       <Route path="/" element={<HomePages />} />
+      <Route path="/register" element={<RegisterPage/>} />
+      <Route path="/forgot" element={<ForgotPassword/>} />
     </Routes>
   );
 }
