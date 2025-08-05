862fd38450d4611c2413c5bdab2fd5e7ddf056ab
SYADRINA-XI-TJKT-3 <sarden082@gmail.com>
2025-08-05 09:06:34 +0700

    update router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,16 @@
 import {Routes, Route } from "react-router-dom";
-import ForgotPasswordPages from "../Pages/ForgotPasswordPage"; // gunakan path relatif yang sesuai
+import ForgotPasswordPages from "../Pages/ForgotPasswordPages";
 import HomePages from "../Pages/HomePages";
+import RegisterPage from "../pages/RegisterPages";
+import LoginPages from "../Pages/LoginPages"
 
 export default function AppRouter() {
   return (
     <Routes>
         <Route path="/" element={<HomePages />} />
         <Route path="/forgot" element={<ForgotPasswordPages />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 }
